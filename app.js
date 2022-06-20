97e64b6e1133e9b6c664bfb12cd6271c8012a577
smoldiel <44263288+smoldiel@users.noreply.github.com>
2022-06-20 18:00:52 +0200

    Chatik upload

---
+++ app.js
@@ -0,0 +1,219 @@
+import express from 'express'
+import http from 'http'
+import cookieParser from 'cookie-parser'
+import chalk from 'chalk'
+import jwt_decode from 'jwt-decode'
+import { OAuth2Client } from 'google-auth-library'
+import { Server } from 'socket.io'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { initializeApp } from 'firebase/app'
+import { getDatabase, onValue, ref, child, push, update, query, limitToLast, get } from "firebase/database"
+import { appendFile } from 'node:fs/promises'
+
+const __filename = fileURLToPath(
+    import.meta.url)
+
+const __dirname = path.dirname(__filename);
+
+const CLIENT_ID = '690604498042-u171b94ejc2t9p8j80dth655n20keism.apps.googleusercontent.com'
+
+const HISTORY_LIMIT = 100
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWM8Dl-nLo_N9eQXt9oHFvlSkKfMli7og",
+    authDomain: "chatik-68e1c.firebaseapp.com",
+    databaseURL: "https://chatik-68e1c-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chatik-68e1c",
+    storageBucket: "chatik-68e1c.appspot.com",
+    messagingSenderId: "645351191132",
+    appId: "1:645351191132:web:0b8987840a8b82799f2c99"
+}
+
+const fireApp = initializeApp(firebaseConfig)
+
+const db = getDatabase(fireApp)
+
+let lastMessage
+let filesFromGeneralChat
+
+const updateLastMessage = data => {
+    lastMessage = data
+}
+const updatefilesFromGeneralChat = data => {
+    filesFromGeneralChat = data
+}
+
+const lastMessageRef = ref(db, 'chats/general/lastMessage')
+onValue(lastMessageRef, snapshot => {
+    const data = snapshot.val()
+    updateLastMessage(data)
+})
+const filesFromGeneralChatRef = ref(db, 'files/general')
+onValue(filesFromGeneralChatRef, snapshot => {
+    const data = snapshot.val()
+    updatefilesFromGeneralChat(data)
+})
+
+const port = process.env.PORT || 3000
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+let users = []
+let connections = []
+
+////////////////////////////////
+////////G-verification//////////
+////////////////////////////////
+const client = new OAuth2Client(CLIENT_ID);
+
+const verifyUserToken = async token => {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    console.log('User ID: ', userid);
+}
+
+const checkUser = (req, res, next) => {
+    res.locals.checkedUser ? next() : res.redirect('/login')
+}
+
+////////////////////////////////
+//////////Middlewares///////////
+////////////////////////////////
+app.use(express.static(__dirname + '/public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(async(req, res, next) => {
+    const token = req.cookies.token
+    try {
+        await verifyUserToken(token)
+        res.locals.checkedUser = true
+    } catch (error) {
+        res.locals.checkedUser = false
+    }
+    next()
+})
+
+////////////////////////////////
+app.set('view engine', 'ejs')
+
+////////////////////////////////
+////Endpoint for G-identity/////
+////////////////////////////////
+app.post('/auth', async(req, res) => {
+    const token = req.body.credential
+    res.cookie('token', token)
+    res.redirect('/chat')
+})
+
+////////////////////////////////
+////////////Routes//////////////
+////////////////////////////////
+app.get('/', checkUser, (req, res) => {
+    res.render('index')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('token')
+    res.redirect('/login')
+})
+
+app.get('/chat', checkUser, (req, res) => {
+    res.render('chat')
+})
+
+app.use((req, res) => {
+    res.status(404)
+    res.send('Not found')
+
+})
+
+////////////////////////////////
+///////Socket is working////////
+////////////////////////////////
+io.sockets.on('connection', socket => {
+    console.log(chalk.bgBlueBright('User is connected'))
+
+    socket.emit('initialize history', { lastMessage })
+
+    socket.emit('send files from chatroom', { filesFromGeneralChat })
+
+    connections.push(socket)
+
+    socket.on('disconnect', data => {
+        connections.splice(connections.indexOf(socket), 1)
+        console.log(chalk.bgRedBright('User is disconnected'));
+    })
+
+    socket.on('get files from chatroom', data => {
+
+    })
+
+    socket.on('get history', data => {
+        const roomHistoryRef = query(ref(db, `messages/${data.room}`), limitToLast(HISTORY_LIMIT))
+        get(roomHistoryRef).then(snapshot => {
+            if (snapshot.exists()) {
+                const roomHistory = snapshot.val()
+                socket.emit('send history', { roomHistory })
+            } else {
+                console.log('No data in firebase')
+            }
+        }).catch(err => {
+            console.error(err)
+        })
+    })
+
+    socket.on('send message', async data => {
+
+        if (data.type !== 'text') {
+            //Загрузить файл на сервер
+            let file
+            data.type == '' ? file = `${data.filename}` : file = `${data.filename}.${data.type}`
+            try {
+                await appendFile(`./public/user_files/${file}`, data.data)
+                console.log("File has been saved");
+            } catch (error) {
+                console.log(error);
+            }
+            data.data = data.filename
+        }
+
+        const messageData = {
+            data: data.data,
+            name: data.name,
+            type: data.type
+        }
+
+        const messageKey = push(child(ref(db), 'message/general')).key
+        const updates = {}
+        updates[`messages/general/${messageKey}`] = messageData
+        updates[`chats/general/lastMessage`] = messageData
+        if (messageData.type !== 'text') {
+            updates[`files/general/${messageKey}`] = messageData
+        }
+        update(ref(db), updates)
+
+        socket.broadcast.emit('add message', { data: messageData.data, name: messageData.name, type: messageData.type })
+        if (messageData.type !== 'text') {
+            io.emit('send files from chatroom', { filesFromGeneralChat })
+        }
+    })
+})
+
+////////////////////////////////
+//////////Start server//////////
+////////////////////////////////
+server.listen(port, () => {
+    console.log(chalk.bgGreen(`Server is running on localhost:${port}`));
+})
---
+++ public/main.js
@@ -0,0 +1,133 @@
+let socket = io.connect()
+let $form = $('#messForm')
+let $userName = $('#name')
+let $message = $('#message')
+let $allMessages = $('#all_mess')
+let $file = $('#formFileSm')
+let $buttonHistory = $('#buttonHistory')
+let $fileBrowserBody = $('#fileBrowserModalBody')
+let $blinking = $('#blinking')
+let file
+
+const blink_speed = 1000
+const t = setInterval(() => {
+    $blinking.css('visibility', $blinking.css('visibility') == 'hidden' ? '' : 'hidden')
+}, blink_speed)
+
+$buttonHistory.click(e => {
+    e.preventDefault()
+    socket.emit('get history', { room: 'general' })
+})
+
+function readFile(input) {
+    file = input.files[0]
+}
+
+const socketGetFilesFromChatroom = room => {
+    socket.emit('get files from chatroom', {
+        room
+    })
+}
+
+const socketSendMessage = (data, name, type, filename = false) => {
+    socket.emit('send message', {
+        data,
+        name,
+        type,
+        filename
+    })
+}
+
+const buildMessageWindow = (data, name, type, my = false) => {
+    let renderedMessage
+    my ? renderedMessage = `<div class="card text-success">` : renderedMessage = `<div class="card">`
+    renderedMessage += `
+        <div class="card-body">
+            <h6 class="card-subtitle mb-2 text-muted">${name}</h6>
+    `
+    if (type != 'text') {
+        let fileForAppend
+        type == '' ? fileForAppend = `${data}` : fileForAppend = `${data}.${type}`
+        renderedMessage += `<a href="${FILE_HOST}/${fileForAppend}" target="_blank" class="link-info">${data}</a>`
+    } else {
+        renderedMessage += `<p class="card-text">${data}</p>`
+    }
+    renderedMessage += `</div></div>`
+    return renderedMessage
+}
+
+const buildFileWindow = (data, name, type, my = false) => {
+    let fileForAppend
+    type == '' ? fileForAppend = `${data}` : fileForAppend = `${data}.${type}`
+    let renderedMessage = `
+        <div class="card card-body">
+            <a href="${FILE_HOST}/${fileForAppend}" target="_blank" class="link-info">${data}</a>
+        </div>
+    `
+    return renderedMessage
+}
+
+const renderList = (objectsFromDB, buildingFunction, appendBody) => {
+    let collection = ``
+    const snapshot = objectsFromDB
+    for (const object in objectsFromDB) {
+        collection += buildingFunction(objectsFromDB[object].data, objectsFromDB[object].name, objectsFromDB[object].type)
+    }
+    appendBody.html('')
+    appendBody.append(collection)
+}
+
+const FILE_HOST = "http://localhost:3000/user_files"
+
+$(function() {
+
+    socketGetFilesFromChatroom('general')
+
+    $form.submit(event => {
+        event.preventDefault()
+        const nameSender = $userName.val() ? $userName.val() : 'Anonymous'
+        if (file) {
+            const fileObject = $file.val().replace(/^.*[\\\/]/, '')
+            const fileName = fileObject.substring(0, fileObject.lastIndexOf('.')) || fileObject
+            let fileType
+            fileObject == fileName ? fileType = '' : fileType = fileObject.substring(fileObject.lastIndexOf('.') + 1, fileObject.length)
+            if (fileType == 'text') {
+                fileType = 'txt'
+            }
+            let reader = new FileReader()
+            reader.readAsArrayBuffer(file)
+            reader.onload = () => {
+                socketSendMessage(reader.result, nameSender, fileType, fileName)
+                $allMessages.append(buildMessageWindow(fileName, nameSender, fileType, true))
+                $allMessages.scrollTop($allMessages[0].scrollHeight)
+            }
+            reader.onerror = () => {
+                console.log(reader.error);
+            }
+        } else {
+            socketSendMessage($message.val(), nameSender, 'text')
+            $allMessages.append(buildMessageWindow($message.val(), nameSender, 'text', true))
+            $allMessages.scrollTop($allMessages[0].scrollHeight)
+            $message.val('')
+        }
+    });
+
+    socket.once('initialize history', data => {
+        $allMessages.prepend(buildMessageWindow(data.lastMessage.data, data.lastMessage.name, data.lastMessage.type))
+    })
+
+    socket.on('send files from chatroom', data => {
+        console.log(data.filesFromGeneralChat)
+        renderList(data.filesFromGeneralChat, buildFileWindow, $fileBrowserBody)
+    })
+
+    socket.on('send history', data => {
+        console.log(data.roomHistory);
+        renderList(data.roomHistory, buildMessageWindow, $allMessages)
+    })
+
+    socket.on('add message', data => {
+        $allMessages.append(buildMessageWindow(data.data, data.name, data.type))
+        $allMessages.scrollTop($allMessages[0].scrollHeight)
+    })
+})
